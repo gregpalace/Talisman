5107cde7026cc699ed586f3473bab6b1852065d4
gregpalace <greg.palasciano@gmail.com>
2020-11-09 13:59:53 -0800

    Add HUD Tests

--- __tests__/client/containers/HudItems.test.tsx
+++ __tests__/client/containers/HudItems.test.tsx
@@ -2,18 +2,47 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 
+import HealthBar from '../../../src/client/components/hud/healthBar';
+import MagicBar from '../../../src/client/components/hud/magicBar';
 import { HudItems } from '../../../src/client/containers/HudItems';
 
 describe('React unit tests', () => {
   let wrapper: any;
+  const showInventory = jest.fn();
+
+  const props = {
+    hp: 100,
+    mp: 100,
+    percentage: 10,
+  };
 
   beforeAll(() => {
-    wrapper = shallow(<HudItems />);
+    wrapper = shallow(<HudItems {...props} showInventory={showInventory} />);
   });
 
   describe('<HudItems />', () => {
-    it('should render correclty', () => {
+    xit('should render correclty', () => {
       expect(toJson(wrapper)).toMatchSnapshot();
+    });
+    it('should render a health and magic bar', () => {
+      expect(wrapper.find(HealthBar)).toBeDefined();
+      expect(wrapper.find(MagicBar)).toBeDefined();
+    });
+    it('should render a healthbar that displays a players health', () => {
+      expect(wrapper.find(HealthBar).prop('hp')).toBe(100);
+      expect(wrapper.find(HealthBar).prop('hp')).not.toBe(0);
+    });
+    it('should render a magic bar that displays a players magic', () => {
+      expect(wrapper.find(MagicBar).prop('mp')).toBe(100);
+      expect(wrapper.find(MagicBar).prop('mp')).not.toBe(0);
+    });
+    it('should render a show inventory button', () => {
+      expect(wrapper.find('button').prop('id')).toBe('inventory-button');
+      expect(wrapper.find('button').prop('onClick')).toBeInstanceOf(Function);
+    });
+    it('should open a modal when the show inventory button is clicked', () => {
+      const button = wrapper.find('button');
+      const mockCallBack = button.prop('showInventory');
     });
   });
 });
