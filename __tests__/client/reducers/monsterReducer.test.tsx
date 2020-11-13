34e291897487a3923a48c395dd9b5ff93816eb0e
gregpalace <greg.palasciano@gmail.com>
2020-11-13 12:47:20 -0800

    Initial MonsterReducer Tests

---
+++ __tests__/client/reducers/monsterReducer.test.tsx
@@ -0,0 +1,26 @@
+import subject from '../../../src/client/reducers/monsterReducer';
+
+describe('Stats reducer tests', () => {
+  let state: any;
+
+  state = {
+    config: {},
+  };
+
+  describe('Reducer main functionality', () => {
+    it('should return default state when given undefined input', () => {
+      expect(subject(undefined, { type: undefined })).toEqual(state);
+    });
+    it('should return a default state when given an undefined action', () => {
+      const action = { type: 'This is a test!' };
+      expect(subject(state, action)).toEqual(state);
+    });
+  });
+
+  describe('RESET', () => {
+    it('should return initial state when game is reset', () => {
+      const action = { type: 'RESET' };
+      expect(subject(state, action)).toMatchObject(state);
+    });
+  });
+});
