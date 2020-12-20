fa421a15dcadc7b49a9ae1d827e8917ebfc15222
gregpalace <greg.palasciano@gmail.com>
2020-12-20 12:47:31 -0500

    Include Dialog Updates and Styling

---
+++ src/client/components/MicroDialog.tsx
@@ -0,0 +1,45 @@
+import React, { useEffect } from 'react';
+
+import '../../stylesheets/microDialog.scss';
+
+const MicroDialog = ({
+  noButton,
+  onClose,
+  children,
+  fullsize,
+  className,
+  onKeyPress,
+}: any) => {
+  useEffect(() => {
+    if (onKeyPress) window.addEventListener('keydown', handleKeyPress);
+    return () => {
+      if (onKeyPress) window.removeEventListener('keydown', handleKeyPress);
+    };
+  }, []);
+
+  function handleKeyPress(event: KeyboardEvent) {
+    // case for 'enter' or 'space' key
+    if (event.keyCode === 13 || event.keyCode === 32) {
+      onKeyPress();
+    }
+  }
+
+  const noSpacing = { top: 0, bottom: 0, left: 0, right: 0 };
+
+  return (
+    <div
+      style={fullsize ? noSpacing : {}}
+      className={`micro-dialog__container white-border ${className || ''}`}
+    >
+      {!noButton && (
+        <button className='micro-dialog__close' onClick={onClose}>
+          <i className={`fa fa-times`} />
+        </button>
+      )}
+
+      {children}
+    </div>
+  );
+};
+
+export default MicroDialog;
